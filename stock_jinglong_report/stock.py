95b259c47ec6172000539b35342ae295f82b7709
lihaihe <lihaihe@hotmail.com>
2015-01-24 16:03:28 +0800

    add sum oders print

--- stock_jinglong_report/stock.py
+++ stock_jinglong_report/stock.py
@@ -19,6 +19,7 @@
 #
 ##############################################################################
 
+from openerp import tools
 from openerp.osv import fields, osv
 from openerp.tools.translate import _
 
@@ -31,3 +32,62 @@ from openerp.tools.translate import _
 #	    'conumber': fields.related('lot_id', 'conumber', type='char', string='conumber', help="conumber"),
 #	    'width': fields.related('lot_id', 'width', type='char', string='width', help="width"),
 #	}
+
+class stock_picking(osv.osv):
+    _inherit= 'stock.picking'
+    _columns = {
+        'move_lines2': fields.one2many('stock.move2', 'picking_id', 'Sum Moves'),
+#	    'conumber': fields.related('lot_id', 'conumber', type='char', string='conumber', help="conumber"),
+#	    'width': fields.related('lot_id', 'width', type='char', string='width', help="width"),
+	}
+
+class stock_move2(osv.osv):
+    _name = "stock.move2"
+    _description = "stock move2"
+    _auto = False
+    _rec_name = 'picking_id'
+
+    _columns = {
+        'picking_id': fields.many2one('stock.move', 'stock picking', readonly=True),
+        'product_id': fields.many2one('product.product', 'product', readonly=True),
+        'product_qty': fields.float('product qty', digits=(16,2), readonly=True),
+        'number': fields.integer('number', readonly=True),
+        'money': fields.float('Price', digits=(16,2), readonly=True),
+        'amount': fields.float('amount', digits=(16,2), readonly=True),
+    }
+    _order = 'picking_id desc'
+
+    def _select(self):
+        select_str = """
+             SELECT c.picking_id::text || '-' || c.product_id::text AS id ,
+					c.picking_id as picking_id ,
+                    c.product_id as product_id,
+                    sum(c.product_qty) as product_qty,
+                    sum(c.number) as number,
+					c.money as money,
+					c.money*sum(c.product_qty) as amount
+        """
+        return select_str
+
+    def _from(self):
+        from_str = """
+               stock_move c
+        """
+        return from_str
+
+    def _group_by(self):
+        group_by_str = """ GROUP BY
+                c.picking_id,
+                c.product_id,
+				c.money
+        """
+        return group_by_str
+
+    def init(self, cr):
+        #self._table = stock_move2
+        tools.drop_view_if_exists(cr, self._table)
+        cr.execute("""CREATE or REPLACE VIEW %s as (
+            %s
+            FROM  %s 
+            %s
+            )""" % (self._table, self._select(), self._from(), self._group_by()))
